a8480ad080c6d24cbbc8ba715a378661daf8c9c2
Admiroo <132172589+Admiroo@users.noreply.github.com>
2024-02-02 23:42:49 -0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,40 @@
+﻿using Exiled.API.Interfaces;
+using InventorySystem.Items.Usables.Scp330;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalaRosa
+{
+    public sealed class Config : IConfig
+    {
+
+        [Description("Wheter the plugin is Enabled or not.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Whether or not debug messages should be shown in the console.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Chances of getting each candy")]
+        public Dictionary<CandyKindID, float> Chances { get; set; } = new()
+        {
+            { CandyKindID.Red, 15.83f },
+            { CandyKindID.Purple, 15.83f },
+            { CandyKindID.Green, 15.83f },
+            { CandyKindID.Blue, 15.83f },
+            { CandyKindID.Yellow, 15.83f },
+            { CandyKindID.Rainbow, 15.83f },
+            { CandyKindID.Pink, 5.02f },
+        };
+
+        [Description("Number of times players can interact with Scp330 before their hands get severed")]
+        public int MaxInteractionAmount { get; set; } = 2;
+
+        [Description("Chance of severing the player's hands after reaching the limit of interactions")]
+        public float SeveringHandsChance { get; set; } = 100f;
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,130 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using MEC;
+using Scp330 = Exiled.Events.Handlers.Scp330;
+using PlayerHandler = Exiled.Events.Handlers.Player;
+using BalaRosa;
+using Exiled.Events.EventArgs.Scp330;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using InventorySystem.Items.Usables.Scp330;
+using Exiled.Events.EventArgs.Player;
+
+namespace BalaRosa
+{
+    public class ConfigurableScp330 : Plugin<Config>
+    {
+        public static ConfigurableScp330 Instance { get; private set; }
+        public override string Name => "ConfigurableScp330";
+        public override string Author => "Admiro";
+        public override string Prefix => "Scp330";
+        public override Version Version => new Version(1, 1, 0);
+        public override Version RequiredExiledVersion => new Version(8, 4, 3);
+        public override PluginPriority Priority => PluginPriority.Medium;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            RegisterEvents();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            UnRegisterEvents();
+            base.OnDisabled();
+        }
+
+        public void RegisterEvents()
+        {
+            Scp330.InteractingScp330 += OnInteractingWithScp330;
+            PlayerHandler.Spawned += PlayerSpawned;
+        }
+        public void UnRegisterEvents()
+        {
+            Scp330.InteractingScp330 -= OnInteractingWithScp330;
+            PlayerHandler.Spawned -= PlayerSpawned;
+        }
+
+        public Dictionary<Player, int> Scp330Interactions = new();
+
+        public void PlayerSpawned(SpawnedEventArgs ev)
+        {
+            if (Scp330Interactions.ContainsKey(ev.Player))
+            {
+                Scp330Interactions.Remove(ev.Player);
+            }
+        }
+
+        public void OnInteractingWithScp330(InteractingScp330EventArgs ev) 
+        {
+            if (ev.Player == null) return;
+
+
+            ev.ShouldSever = false;
+
+            if (!Scp330Interactions.ContainsKey(ev.Player))
+            {
+                Scp330Interactions.Add(ev.Player, 1);
+            }
+            else
+            {
+                Scp330Interactions[ev.Player]++;
+            }
+
+            if (Scp330Interactions[ev.Player] > Instance.Config.MaxInteractionAmount)
+            {
+                if ((UnityEngine.Random.value * 100) <= Instance.Config.SeveringHandsChance)
+                {
+                    ev.ShouldSever = true;
+                    Scp330Interactions.Remove(ev.Player);
+                    return;
+                }
+            }
+
+            List<CandyKindID> List = Instance.Config.Chances.Keys.ToList();
+
+            float RandomNumber = UnityEngine.Random.value * Instance.Config.Chances.Values.Sum();
+            
+            if (RandomNumber <= Sum(1))
+            {
+                ev.Candy = List[0];
+            }
+            else if (RandomNumber <= Sum(2))
+            {
+                ev.Candy = List[1];
+            }
+            else if (RandomNumber <= Sum(3))
+            {
+                ev.Candy = List[2];
+            }
+            else if (RandomNumber <= Sum(4))
+            {
+                ev.Candy = List[3];
+            }
+            else if (RandomNumber <= Sum(5))
+            {
+                ev.Candy = List[4];
+            }
+            else if (RandomNumber <= Sum(6))
+            {
+                ev.Candy = List[5];
+            }
+            else if (RandomNumber <= Sum(7))
+            {
+                ev.Candy = List[6];
+            }
+        }
+
+        public float Sum(int value)
+        {
+            List<float> values = Instance.Config.Chances.Values.ToList();
+            float result = 0;
+            for (int i = 0; i < value; i++)
+            {
+               result += values[i];
+            }
+            return result;
+        }
+    }
+}
